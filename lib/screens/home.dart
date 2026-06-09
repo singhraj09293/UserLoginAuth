127eceed5793c1dde4b22b018f44dda2ec9c5a9d
Raj singh <singhraj092932@gmail.com>
2026-06-09 21:34:45 +0530

    Added stream builder which listen automatically using firebase

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,91 +12,146 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Login? user;
   TextEditingController newName = TextEditingController();
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration(seconds: 2), () {
-      if (mounted) getUserData();
-    });
-  }
-
-  getUserData() async {
-    final uid = FirebaseAuth.instance.currentUser!.uid;
-    print('UID: $uid');
-
-    DocumentSnapshot doc = await FirebaseFirestore.instance
-        .collection('users')
-        .doc(uid)
-        .get();
-
-
-      if (doc.exists && mounted) {
-        setState(() {
-          user = Login.fromMap(doc.data() as Map<String, dynamic>);
-        });
-      }
-
-  }
+  TextEditingController pass = TextEditingController();
+  bool isPasswordVisible = false;
 
   updateName() async {
     final uid = FirebaseAuth.instance.currentUser!.uid;
     await FirebaseFirestore.instance.collection('users').doc(uid).update({
       'fullName': newName.text.trim(),
     });
-    getUserData();
+  }
+
+  delteAcc(String password) async {
+    final user = FirebaseAuth.instance.currentUser!;
+    final uid = user.uid;
+    try {
+      AuthCredential credential = EmailAuthProvider.credential(
+        email: user.email!,
+        password: password,
+      );
+      await user.reauthenticateWithCredential(credential);
+      await FirebaseFirestore.instance.collection('users').doc(uid).delete();
+      await user.delete();
+    } catch (e) {
+      Text('Error $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Welcome ${user?.fullName ?? 'User'}'),
-            SizedBox(height: 20),
-            GestureDetector(
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (_) => AlertDialog(
-                    title: Text('Change the name you want'),
-                    content: TextField(
-                      controller: newName,
-                      decoration: InputDecoration(hintText: 'Enter Name'),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (_) => AlertDialog(
+                  title: Text('Enter password to confirm'),
+                  content: TextField(
+                    controller: pass,
+                    obscureText: !isPasswordVisible,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isPasswordVisible = !isPasswordVisible;
+                          });
+                        },
+                        icon: isPasswordVisible
+                            ? Icon(Icons.visibility)
+                            : Icon(Icons.visibility_off),
+                      ),
                     ),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: Text('Cancel'),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          updateName();
-                          Navigator.pop(context);
-                        },
-                        child: Text('Change'),
-                      ),
-                    ],
                   ),
-                );
-              },
-              child: Text('Change Name?',style: TextStyle(color: Colors.blueAccent),),
-            ),
-            SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-                await GoogleSignIn().signOut();
-              },
-              child: Icon(Icons.logout),
-            ),
-          ],
-        ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Cancel'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        delteAcc(pass.text.trim());
+                        Navigator.pop(context);
+                      },
+                      child: Text('Delete'),
+                    ),
+                  ],
+                ),
+              );
+            },
+            icon: Icon(Icons.delete),
+          ),
+        ],
+      ),
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .doc(FirebaseAuth.instance.currentUser!.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            Login user = Login.fromMap(
+              snapshot.data!.data() as Map<String, dynamic>,
+            );
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Welcome ${user.fullName}'),
+                  SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (_) => AlertDialog(
+                          title: Text('Change the name you want'),
+                          content: TextField(
+                            controller: newName,
+                            decoration: InputDecoration(hintText: 'Enter Name'),
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text('Cancel'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                updateName();
+                                Navigator.pop(context);
+                              },
+                              child: Text('Change'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Change Name?',
+                      style: TextStyle(color: Colors.blueAccent),
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await FirebaseAuth.instance.signOut();
+                      await GoogleSignIn().signOut();
+                    },
+                    child: Icon(Icons.logout),
+                  ),
+                ],
+              ),
+            );
+          }
+          return CircularProgressIndicator();
+        },
       ),
     );
   }
