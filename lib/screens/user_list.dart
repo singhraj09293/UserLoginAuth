8324791c290f7b2b5810f30877c52cf24a8b6fc3
Raj singh <singhraj092932@gmail.com>
2026-06-10 11:56:29 +0530

    added View user and search user fullName

--- lib/screens/user_list.dart
+++ lib/screens/user_list.dart
@@ -12,7 +12,17 @@ class _UserListState extends State<UserList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('User List')),
+      appBar: AppBar(
+        title: Text('User List'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showSearch(context: context, delegate: UserSearchDelegate());
+            },
+            icon: Icon(Icons.search),
+          ),
+        ],
+      ),
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance.collection('users').snapshots(),
         builder: (context, snapshot) {
@@ -32,6 +42,62 @@ class _UserListState extends State<UserList> {
           );
         },
       ),
+    );
+  }
+}
+
+class UserSearchDelegate extends SearchDelegate {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(onPressed: () => query = '', icon: Icon(Icons.clear_outlined)),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () => close(context, null),
+      icon: Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return _buildUserList();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return _buildUserList();
+  }
+
+  Widget _buildUserList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('users')
+          .where('fullName', isGreaterThanOrEqualTo: query)
+          .where('fullName', isLessThan: query + 'z')
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return Center(child: Text('User not found'));
+        }
+        return ListView.builder(
+          itemCount: snapshot.data!.docs.length,
+          itemBuilder: (context, index) {
+            Map<String, dynamic> data =
+                snapshot.data!.docs[index].data() as Map<String, dynamic>;
+            return ListTile(
+              title: Text(data['fullName']),
+              subtitle: Text(data['email']),
+            );
+          },
+        );
+      },
     );
   }
 }
